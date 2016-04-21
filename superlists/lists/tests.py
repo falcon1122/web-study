fafad1a3bf55654f62634d9f183da94d765a7cad
falcon1122 <falcon1122@163.com>
2016-04-21 23:03:48 +0800

    test urls.py and python35 manage.py test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,10 @@
+#_*_ coding:utf-8 _*_
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    def test_root_resolves_to_home_page_view(self):
+#解析url
+        found = resolve('/')
+        self.assertEqual(found.func,home_page)
