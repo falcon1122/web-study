dc67e03f8b5a39b19328c4f4f1e4a9b96aa0d180
falcon1122 <falcon1122@163.com>
2016-04-25 22:20:48 +0800

    modify functional_tests.py add a def to reduse code

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -8,8 +8,15 @@ class NewVistorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 #发现网站,打开首页
         self.browser.get('http://localhost:8000')
@@ -24,10 +31,16 @@ class NewVistorTest(unittest.TestCase):
         inputbox.send_keys('buy some food')
 #输入回车更新
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:buy some food',[row.text for row in rows])
+        
+        self.check_for_row_in_list_table('1:buy some food')
+#又增加一个待办项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        input.sent_keys('use food do dinner')
+        input.sent_keys(Keys.ENTER)
+#测试看是否显示2个
+        self.check_for_row_in_list_table('1:buy some food')
+        self.check_for_row_in_list_table('2:use food do dinner')
+        	
 #显示一个文本框，输入其他待办事项
         
         self.fail('Finish the test')
